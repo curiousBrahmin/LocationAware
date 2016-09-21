09f0c355377173642a2c5d69c2df1b708b921959
curiousBrahmin <Rambo@rambos-imac.hsd1.tn.comcast.net>
2016-09-21 18:29:17 -0500

    Initial Commit

---
+++ LocationAware/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LocationAware
+//
+//  Created by Rambo on 9/21/16.
+//  Copyright © 2016 Rambo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
